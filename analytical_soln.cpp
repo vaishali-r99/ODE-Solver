70fa83a6d9d05a1213ee8eff14da9c5dae366cb9
Vaishali Ravishankar <vaishali.ravishankar@tum.de>
2021-12-09 20:40:13 +0100

    Update analytical_soln.cpp

--- analytical_soln.cpp
+++ analytical_soln.cpp
@@ -1,3 +1,11 @@
+
+// Computing the analytical solution
+//The analytical solution calculated manually using integration for the given ODE: y=y0*e^(0.25*t^4-1.5*t)
+//The two input parameters for the function are y0 and t values and returns the y value
+
+
+
+
 #include<iostream>
 #include<vector>
 #include<cmath>
