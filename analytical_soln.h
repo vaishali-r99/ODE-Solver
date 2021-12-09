fb176cdfd936fd943860bc8a40cc7068c08edb4e
Vaishali Ravishankar <vaishali.ravishankar@tum.de>
2021-12-09 20:33:46 +0100

    Update analytical_soln.h

---
+++ analytical_soln.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#ifndef ANALYTICAL_SOLN_H
+#define ANALYTICAL_SOLN_H
+
+double analytical_soln(double, double );
+
+
+#endif
--- f1.h
+++
@@ -1,9 +0,0 @@
- #include<iostream>
- #include<vector>
-#ifndef F1_H
-#define F1_H
-
-double f1(double , double );
-
-#endif
-
