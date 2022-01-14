0031b61844f95f424c3b60d0ecae1ebfe4ac4e3a
Nandini Joshi <ge32xip@mytum.de>
2022-01-14 23:37:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,69 +1,46 @@
-/*This is the code for ODE solver using Explicit Euler Method.
-The ODE used in the following code is dy/dt= y*t^3 -1.5*y
-the initial value y(0)=1;
-Here, we compute the Explicit Euler Solution and the analytical solution
-*/
-
-
-/* Input for the values of time step (h) and end time (t_end) is taken from the user
-y_0 is initialized 
-using exp_euler () fn, result is calculated and stored in y_result vector
-using analytical_soln () fn, analytical solution is obtained and stored in analytical_result vector
-err defined as the absolute value of difference between analytical solution and computed result
-here in test case at each timestep reading, err is computed and a counter cnt is used check if the err>0.1 [assumption]
-if the err criteria is true for more than half the results, then it is flaged as an inefficient solution and inputs should be changed.
-
-
-*/
-
-
-
-
-#include<iostream>
+#include <iostream>
 #include<vector>
-#include<cmath>
-#include"exp_euler.h"
-#include"analytical_soln.h"
+#include<math.h>
+#include <memory>
+
+#include "scheme.h"
+#include"ExplicitEuler.h"
+#include"ImplicitEuler.h"
+
+
+void implement_solver(std::unique_ptr<scheme> &s)
+{
+    s->Solver();
+}
 
 
 int main()
 {
-    double t,h,y,t_end,y_0;
-    std::cout<<"\n Please enter the time step: ";
-    std::cin>>h;
-    std::cout<<"\n Enter the end time:";
-    std::cin>>t_end;
-    std::cout<<"\n Enter the initial value:";
-    std::cin>>y_0;
+    int choice;     //choice- to select the solving method
+    double y0,h,t_end; 
     
-    std::vector<double> y_result;
-    y_result=exp_euler(y_0,h,t,t_end);
-
-        double err;
-    int cnt=0;
-    double t_a=0.0;
-    double a_res;
-    std::vector<double> analytical_result;
-    while(t_a<=t_end)
-    {   
-        analytical_result.push_back(analytical_soln(y_0, t_a));
-        t_a+=h;
-    }
-     std::cout<<"\n Exp. Euler Soln. \t Analytical soln. : ";
-    for(auto i=0;i<y_result.size();i++)
-    {
-        std::cout<<"\n"<<y_result[i]<<"\t"<<analytical_result[i];
-        err= fabs(analytical_result[i]-y_result[i]);
-        if( err>0.1)
-         cnt++;
-
-    }
-    if(cnt>(y_result.size()/2))
-        std::cout<<"\n Test case failed! Method not suitable for the given problem"; 
+    //tried with y0=1
+    y0=1;
+    std::cout<<"Enter the interval size h= ";
+    std::cin>>h;
+    std::cout<<"Enter the end time T_end= ";
+    std::cin>>t_end;
+    std::cout<<"Enter your choice for solving the ODE 1. Explicit Euler 2. Implicit Euler ";
+    std::cin>>choice;
+    std::unique_ptr<scheme> myScheme;
+    if(choice==1)
+        {
+            myScheme = std::make_unique<ExplicitEuler>(y0,h,t_end);
+            implement_solver(myScheme);
+        }
+    else if(choice==2)
+        {
+            myScheme = std::make_unique<ImplicitEuler>(y0,h,t_end);
+            implement_solver(myScheme);
+        }
     else
-         std::cout<<"\n Test case passed! Method suitable for the given problem";;
+        {std::cout<<"Choice does not exist";}
 
-
+      
+    
 }
-
-
