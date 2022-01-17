03cb4dca622ddd7eda3b4ee45a88f029751c26a4
Vaishali Ravishankar <vaishali.ravishankar@tum.de>
2022-01-17 12:28:03 +0100

    Update ImplicitEuler.cpp

--- ImplicitEuler.cpp
+++ ImplicitEuler.cpp
@@ -21,46 +21,43 @@ void ImplicitEuler::Solver() const
 
 std::vector<double> y,time,y_analytical; // y- stores the result vector, time- time vector, y_analytical- stores the analytical solution
 //The analytical solution calculated manually using integration for the given ODE: y=y0*e^(0.25*t^4-1.5*t)
-double yd,y_res,ya,t; //
+double yd,y_res,ya,t,yi; //
 int N; // size of vector solution
 int cnt=0;  // counter to check for number of error terms
 y_res = _y0; 
 t = 0.0;
 N = _t_end/_h + 1;
 double err=0.0; // error
-/*for (int i = 0; i<N; i++)
+time.push_back(t);
+y_analytical.push_back(_y0);
+y.push_back(_y0);
+yi = _y0;
 
-{   ya=_y0*exp((pow(t,4)/4)-1.5*t);     // analytical term
-    y_analytical.push_back(ya);  
-    t=t+_h;                   
-    yd = newton(_h,y_res+_h*f(y_res,t),t);                  //newton solution
-    time.push_back(t);
-    y_res= y_res+_h*f(yd,t);                //implicit solution
-    y.push_back(y_res);
-}*/
 for ( int i = 1; i<N; i++)
 
-{   t=t+_h;
-    ya=_y0*exp((pow(t,4)/4)-1.5*t);     // analytical term
-    //ya=sqrt(pow(_y0,2)/(1-2*pow(_y0,2)*t));
-    y_analytical.push_back(ya);                     
-    yd = newton(_h,y_res+_h*f(y_res,t),t); //newton solution
+{   //std::cout<<"\n y_res "<<y_res<<"\t"<<" guess value \t"<<y_res+_h*f(y_res,t)<<" Time step \t"<<t<<"\n";
+    yd = newton(_h,yi,yi+_h*f(yi,t),t); //newton solution
+    t=t+_h;
     time.push_back(t);
-    y_res= y_res+_h*f(yd,t);                //implicit solution
-    y.push_back(y_res);
+    ya=_y0*exp((pow(t,4)/4)-1.5*t);     // analytical term
+    y_analytical.push_back(ya);   
+    //std::cout<<" Approximate  solution "<<y_res<<"\n";
+    y.push_back(yd);
+    yi = yd;
 }    
+
 std::ofstream file;
 file.open ("results.txt", std::ios::app);
 
-std::cout<<"The solution by Implicit Euler method is \n";
-file<<"The solution by Implicit Euler method is \n";
-std::cout<<"Analytical Solution \t"<<"Implicit Scheme Solution \n";
+std::cout<<"\n The solution by Implicit Euler method is \n";
+file<<"\n The solution by Implicit Euler method is \n";
+std::cout<<"Time \t"<<"Analytical Solution \t"<<"Implicit Scheme Solution \n";
 file<<"Analytical Solution \t"<<"Implicit Scheme Solution \n";
 for (int i = 0; i<N; i++)
 {   err=fabs(y[i]-y_analytical[i]);       //check for the error between the analytical solution and scheme result
     if(err>0.1)                    //if error> threshold value, increment counter
         cnt++;    
-    std::cout<<y_analytical[i]<<"\t"<<y[i]<<"\n";
+    std::cout<<time[i]<<"\t"<<y_analytical[i]<<"\t"<<y[i]<<"\n";
     file<<y_analytical[i]<<"\t"<<y[i]<<"\n";
 }
 
