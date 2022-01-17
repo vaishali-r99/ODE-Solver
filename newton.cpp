af19fd80bd69e2bcaabf7544b4a3108134b14364
Vaishali Ravishankar <vaishali.ravishankar@tum.de>
2022-01-17 11:43:05 +0100

    Update newton.cpp

--- newton.cpp
+++ newton.cpp
@@ -8,14 +8,18 @@ double newton(double _h, double y,double t)
     double G,dG;
     double epsilon = 0.1;       // accuracy parameter considered in this case
     double yi = y;                  
-    double e = 0;    
-    G= y*pow(t,3)-1.5*y ;    //function G
-    dG= 3*y*pow(t,2);               // error term used here in the Newton Iteration y_n+1 = y_n-G_n/dG_n
-          //function dG
+    double e = 0; 
+    int i = 0;   
+
     while(abs(e)>epsilon)
     {   
-        e = (y-(_h*G+yi)/(1-_h*dG));
+        G= y*pow(t,3)-1.5*y ;    //function G
+        dG=  1 - _h*(pow(t,3)-1.5);               // error term used here in the Newton Iteration y_n+1 = y_n-G_n/dG_n
+        e = G/dG
         y = y-e;
+        i=i+1;
+        if(i>=100)
+        break;
     }
 
     return y;                   // return the value to Implicit Euler
