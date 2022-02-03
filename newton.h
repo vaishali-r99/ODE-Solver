406dba52794b7488041b0a46a1081831703e3c2b
Nandini Joshi <ge32xip@mytum.de>
2022-02-03 22:38:04 +0100

    Upload New File

---
+++ newton.h
@@ -0,0 +1,15 @@
+#ifndef NEWTON_H
+#define NEWTON_H
+#include "ODE.h"
+#include "ODE1.h"
+#include "ODE2.h"
+#include "ODE3.h"
+#include "ODE4.h"
+
+double newton(double, double, double,double);
+    //double newton(double _h, double yi, double y,double t,ODE _ode) 
+
+
+
+
+#endif
